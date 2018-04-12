0dc926fc3c8ac17af4bc382a96e2b4e5fc44acf5
continuestory <32128132+continuestory@users.noreply.github.com>
2018-04-12 19:38:03 +0800

    Initial Commit

---
+++ homework3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  homework3
+//
+//  Created by CScent on 18/4/12.
+//  Copyright (c) 2018年 CScent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
